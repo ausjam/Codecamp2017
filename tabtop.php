dc00e18ca75d8469df2207664736cfde68a22ccf
Ausjam <willthe21@gmail.com>
2017-04-21 17:10:36 -0600

    footer update

--- tabtop.php
+++ tabtop.php
@@ -4,13 +4,13 @@ echo "<div class=\"tabtop\">
 
 <!-- This div is to hold the navigation buttons at the start of the page -->
 
-<button type=\"button\" name=\"Home\">Home</button>
+<button onclick="location.href = 'www.exchangememes.com';" type=\"button\" name=\"Home\">Home</button>
 
-<button type=\"button\" name=\"Exchange\">Exchange</button>
+<button onclick="location.href = 'www.exchangememes.com/exchangepage.php';" type=\"button\" name=\"Exchange\">Exchange</button>
 
-<button type=\"button\" name=\"Account\">Account</button>
+<button onclick="location.href = '';" type=\"button\" name=\"Account\">Account</button>
 
-<button type=\"button\" name=\"Login\">Login</button>
+<button onclick="location.href = '';" type=\"button\" name=\"Login\">Login</button>
 
 </div>"
 
