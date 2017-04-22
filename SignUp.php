b9fa8e94fe5a5961b09e1c85650697f175130892
Rion1337 <ryn.decker@gmail.com>
2017-04-21 20:10:02 -0600

    Added user authentication and magic.

--- SignUp.php
+++ SignUp.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-/* Creates and assings a session token */
+/* Creates and assings a session token */ // (Hopefully)
 $form_token = md5(uniqid('auth_',true));
 $_SESSION['form_token'] = $form_token;
 
@@ -12,20 +12,18 @@ $_SESSION['form_token'] = $form_token;
 	<head>
 		<script src="scripts/gen_validator4.js" type="text/javascript"></script>
 	</head>
-	<div class="signup">
-		<h1>Account Creation</h1>
+	<div class="loginform">
+		<h1>Create Account</h1>
 		<body>
-			<form action="adduser.php" method="post" id="account_creation">
+			<form action="adduser.php" method="post" id="create_account">
 				<fieldset>
 					<p>
 						<input type="text" name="user_name" value="" maxlength="20"/>
 						<label for="user_name">Username</label>
-					</p>
-					<p>
+					</p><p>
 						<input type="password" name="user_password" value="" maxlength="40"/>
 						<label for="user_password">Password</label>
-					</p>
-					<p>
+					</p><p>
 						<input type="password" name="user_password_confirm" value="" maxlength="40"/>
 						<label for="user_passsword_confirm">Confirm Password</label>
 					</p>
@@ -47,6 +45,10 @@ $_SESSION['form_token'] = $form_token;
 
 				formvalid.addValidation("user_name","maxlen=20",
 						"Your username may not be longer than 20 characters");
+				formvalid.addValidation("user_password","maxlen=40",
+						"Your password may not be longer than 40 charachers");
+				formvalid.addValidation("user_password_confirm", "maxlen=40",
+						"Your password may not be longer than 40 characters");
 
 				formvalid.addValidation("user_name","alnum",
 						"Your username may only contain letters and numbers");
--- login_auth.php
+++ login_auth.php
@@ -10,63 +10,62 @@ $password = "VerySecureSuchData";
 $dbname = "MemeExchange";
 $itemtable = "user_auth";
 
-if(isset($_POST['create']))
+if(isset($_POST['login']))
 {
 	$validator = new FormValidator();
 
 	$validator->addValidation("user_name","req","You must enter a username");
 	$validator->addValidation("user_password","req","You must enter a password");
-	$validator->addValidation("user_password_confirm","req","You must confirm your password");
 
 	$validator->addValidation("user_name","maxlen=20",
 			"Your username may not be longer than 20 characters");
 	$validator->addValidation("user_password","maxlen=40",
 			"Your password may not be longer than 40 characters");
-	$validator->addValidation("user_password_confirm","maxlen=40",
-			"Your password may not be longer than 40 characters");
 
 	$validator->addValidation("user_name","alnum",
 			"Your username may only contain letters and numbers");
-	$validator->addValidation("user_password","eqelmnt=user_password_confirm",
-			"Your password does not match");
 	if($validator->ValidateForm())
 	{
 		$user_name = filter_var($_POST['user_name'], FILTER_SANITIZE_STRING);
 		$user_password = filter_var($_POST['user_password'], FILTER_SANITIZE_STRING);
-		$user_salt = substr(md5(uniqid("",true)),0,20);
-		//$user_hash = return_hash($user_password, $user_salt);
-		$user_hash = hash('sha256', $user_password); //Temp Fix
-		$user_hash .= $user_salt;
-		$user_hash = hash('sha256', $user_hash);
 
 		$db = mysqli_connect($servername, $username, $password, $dbname);
 
-		//Checks if username already exists
-		$query = "SELECT user_name FROM $itemtable WHERE user_name='$user_name'";
+		$query = "SELECT user_salt, user_hash FROM $itemtable WHERE user_name='$user_name'";
 		$result = mysqli_query($db, $query);
 		if($result && mysqli_num_rows($result) > 0)
 		{
-			//Username is taken
-			echo 'Username is already taken';
+			mysqli_data_seek($result, 0);
+			$user_info = mysqli_fetch_row($result);
+			$user_salt = $user_info[0];
+			$user_hash = $user_info[1];
+
+			//$user_hash = return_hash($user_password, $user_info[0]));
+			$test_hash = hash('sha256', $user_password); //Temp Fix
+			$test_hash .= $user_salt;
+			$test_hash = hash('sha256', $test_hash);
+
+			//echo $test_hash;
+			//echo $user_hash;
+
+			if($test_hash == $user_hash)
+			{
+				$auth_token = hash("sha512",uniqid("",true));
+
+				$query = "UPDATE $itemtable SET auth_token = '$auth_token' WHERE
+						user_name='$user_name'";
+				mysqli_query($db, $query);
+				setcookie("auth_token", $auth_token);
+
+				echo 'Logged in Successfully';
+				//Login Successful
+			}
 		}
 		else
 		{
-			//Inserts User into database
-			$query = "UPDATE $itemtable (user_name, user_salt, user_hash)
-					VALUES ('$user_name', '$user_salt', '$user_hash')";
-			if(!mysqli_query($db, $query))
-			{
-				echo 'Failed to insert into database';
-				//Failed to insert into database
-			}
-			else
-			{
-				echo 'Account registered successfully';
-				//Success
-			}
+			echo 'Username or Passsword is incorrect';
+			//No user registered with that name
 		}
-
-		mysqli_close($db);
 	}
 	else
 	{
