796da1b0cb9e22fb1d504187cda769bee7250e73
Ausjam <willthe21@gmail.com>
2017-04-21 18:52:20 -0600

    Fixed the logo size

--- tabtop.php
+++ tabtop.php
@@ -4,6 +4,10 @@ echo "<div class=\"tabtop\">
 
 <!-- This div is to hold the navigation buttons at the start of the page -->
 
+<img src=\"Logo.png\" alt=\"Meme Exchange\">
+
+<p>MEME EXCHANGE</p>
+
 <button type=\"button\" name=\"Home\">Home</button>
 
 <button type=\"button\" name=\"Exchange\">Exchange</button>
