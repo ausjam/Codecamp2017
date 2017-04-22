5db4241ec74640f1773f4504532b050a86ac9181
Ausjam <willthe21@gmail.com>
2017-04-21 23:12:11 -0600

    Started account page formating

--- Account.php
+++ Account.php
@@ -1,3 +1,5 @@
+<html>
+
 <?php
 
 require_once 'PHP/auth.php';
@@ -17,3 +19,60 @@ else
 }
 
 ?>
+
+<?php
+
+include 'tabtop.php';
+
+?>
+
+<div class="accountquote">
+
+<p>"Price is what you pay; value is what you get.
+	Whether we're talking about stocks or memes,
+	I like buying quality content when it is marked down."
+	 - A Great Man</p>
+
+</div>
+
+<div class="accountcontainer">
+
+</div>
+
+<div class="graphs">
+
+
+
+</div>
+
+<div class="portfolio">
+
+
+
+</div>
+
+<div class="acctotals">
+
+<p>Account ------</p>
+
+<p>Current Account Balance</p>
+
+<p>Current Highest Stock</p>
+
+<p>Total Investment</p>
+
+<p>Submit a Meme</p>
+
+</div>
+
+</div>
+
+</body>
+
+<?php
+
+include 'footer.php';
+
+?>
+
+</html>
